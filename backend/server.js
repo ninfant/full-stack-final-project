695c6aebd008cd5d8e0795d71938164a7e18207d
ninfant <noilsa@icloud.com>
2025-05-14 12:47:41 +0300

    general setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
 import flagRoutes from "./routes/featureFlagRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import customerRegionRoutes from "./routes/customerRegionRoutes.js";
@@ -8,14 +10,17 @@ import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 dotenv.config();
 
+// Estas dos líneas para que __dirname funcione con ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "apikey"],
   })
@@ -28,6 +33,18 @@ app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is live!");
+});
+// Servir los archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// // Catch-all route para que React maneje rutas del frontend
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+// });
+
+// Catch-all route para que React maneje rutas del frontend
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.listen(PORT, () => {
