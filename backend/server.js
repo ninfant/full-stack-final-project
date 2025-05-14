51f08b8e8a4cac1351bfc30587f14c4068826fdf
ninfant <noilsa@icloud.com>
2025-05-14 10:48:49 +0300

    gitignore update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "apikey"],
   })
--- micro-service-demo/ms-backend/routes/demoRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { demo1Handler } from "../controllers/demoController.js";
-
-const router = express.Router();
-
-router.get("/demo1", demo1Handler);
-
-export default router;
